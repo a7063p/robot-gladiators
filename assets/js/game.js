d730f23df31de0b21e218fa3494210c33d681271
andrew <ap@ampiusa.com>
2020-12-20 22:44:26 -0800

    first step in robot gladiator

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,36 @@
 
-var playerName = window.prompt("whats your robots name?");
+var playerName = window.prompt("What is your robots name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
-console.log("this logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("our robots name is " + playerName);
+var enemyName = "Roborto";
+var enemyHealth = 5;
+var enemyAttack = 12;
 
 
+var fight = function() {
+    //Alert Players that they are starting the round
+    window.alert("Welcome to the Robot Gladiators!"); 
 
-//fight();
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth" and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+    //log a resulting message to the console so we know it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
+    playerHealth = playerHealth - enemyAttack;
+    //log a resulting message to the console so we know that it worked
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining");
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+};
+
+ 
+
+
+fight();
 
 
 
