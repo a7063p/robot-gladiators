2844d6d36e03575b339af865c6833664855b7538
andrew <ap@ampiusa.com>
2020-12-21 06:14:47 -0800

    added conditionals for robots health and death status

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@ var playerHealth = 100;
 var playerAttack = 10;
 
 var enemyName = "Roborto";
-var enemyHealth = 5;
+var enemyHealth = 50;
 var enemyAttack = 12;
 
 
@@ -24,6 +24,12 @@ var fight = function() {
         window.alert(enemyName + " has died!");
     } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    if (playerHealth <=0) {
+        window.alert(playerName + " has died!");
+    } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
     }
 };
 
