1160ab6ce72b6742196bdac1d0966f99eb5b90a5
andrew <ap@ampiusa.com>
2020-12-20 17:09:24 -0800

    console.log example

---
+++ assets/js/game.js
@@ -0,0 +1,48 @@
+
+var playerName = window.prompt("whats your robots name?");
+
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("our robots name is " + playerName);
+
+
+
+//fight();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
